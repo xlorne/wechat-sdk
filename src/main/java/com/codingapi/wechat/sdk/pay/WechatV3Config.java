dccc2b2cd923f6f20ee3c64cecbce1b35ab85610
1991wangliang@gmail.com <1991wangliang@gmail.com>
2023-03-22 19:33:51 +0800

    add wechat pay sdk

--- src/main/java/com/codingapi/wechat/sdk/pay/WechatV3Config.java
+++ src/main/java/com/codingapi/wechat/sdk/pay/WechatV3Config.java
@@ -1,44 +1,70 @@
 package com.codingapi.wechat.sdk.pay;
 
-import lombok.AllArgsConstructor;
+import com.wechat.pay.java.core.Config;
+import com.wechat.pay.java.core.RSAAutoCertificateConfig;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class WechatV3Config {
 
     /**
      * 商户编号
      */
+    @Setter
+    @Getter
     private String mchId;
 
     /**
      * 应用Id
      */
+    @Setter
+    @Getter
     private String appid;
 
     /**
      * 证书编号
      */
+    @Setter
+    @Getter
     private String mchSerialNo;
 
     /**
      * apiV3Key
      */
+    @Setter
+    @Getter
     private String apiV3Key;
 
     /**
      * 证书私钥
      */
+    @Setter
+    @Getter
     private String privateKeys;
 
     /**
      * 支付回调地址
      */
+    @Setter
+    @Getter
     private String notifyUrl;
+
+
+    private Config config;
+
+    public Config getWechatPayConfig(){
+        if(config==null) {
+            this.config = new RSAAutoCertificateConfig.Builder()
+                    .merchantId(mchId)
+                    .privateKey(privateKeys)
+                    .merchantSerialNumber(mchSerialNo)
+                    .apiV3Key(apiV3Key)
+                    .build();
+        }
+        return config;
+    }
+
+
 
 }
