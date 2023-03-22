26bc05d10290b382cd6c6f125b4d045b298f8a00
lorne <1991wangliang@gmail.com>
2023-03-22 22:18:10 +0800

    update pom

--- src/main/java/com/codingapi/wechat/sdk/cgibin/api/BaseInformationApi.java
+++ src/main/java/com/codingapi/wechat/sdk/cgibin/api/BaseInformationApi.java
@@ -4,14 +4,18 @@ import com.alibaba.fastjson2.JSON;
 import com.codingapi.wechat.sdk.cgibin.CgiBinClient;
 import com.codingapi.wechat.sdk.oauth2.dto.AccessToken;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class BaseInformationApi {
 
     private final CgiBinClient cgiBinClient;
 
+    private final Map<String,AccessToken.Response> cache = new ConcurrentHashMap<>();
+
     public BaseInformationApi(CgiBinClient cgiBinClient) {
         this.cgiBinClient = cgiBinClient;
     }
-
 
     /**
      * 获取access_token
@@ -20,11 +24,24 @@ public class BaseInformationApi {
      * @return 获取Access token
      */
     public AccessToken.Response token(String grantType){
+        AccessToken.Response token = cache.get(grantType);
+        if(token==null) {
+            token = getToken(grantType);
+            cache.put(grantType,token);
+        }
+        if(token.isSuccess()&&token.isExpire()){
+            token = getToken(grantType);
+            cache.put(grantType,token);
+        }
+        return token;
+    }
+
+    private AccessToken.Response getToken(String grantType){
         String appid = cgiBinClient.getAppId();
         String secret = cgiBinClient.getAppSecret();
-        String url = "/cgi-bin/token?grant_type="+grantType+"&appid="+appid+"&secret="+secret;
+        String url = "/cgi-bin/token?grant_type=" + grantType + "&appid=" + appid + "&secret=" + secret;
         String response = cgiBinClient.get(url);
-        return JSON.parseObject(response,AccessToken.Response.class);
+        return JSON.parseObject(response, AccessToken.Response.class);
     }
 
 
